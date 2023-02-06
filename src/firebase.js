5a910c824e86767b29a18e4a1952e328f97e1ca4
metaphore520 <119277137+metaphore520@users.noreply.github.com>
2023-02-06 18:29:26 +0600

    Firebase Config Updated

---
+++ src/firebase.js
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+});
+
+/*
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsU8bQiQo_u8Rt5faGmU7utMEQkC_hfRc",
+  authDomain: "react-quiz-dev-61b47.firebaseapp.com",
+  projectId: "react-quiz-dev-61b47",
+  storageBucket: "react-quiz-dev-61b47.appspot.com",
+  messagingSenderId: "565275689202",
+  appId: "1:565275689202:web:89ec2cee3b47391340d06b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+*/
